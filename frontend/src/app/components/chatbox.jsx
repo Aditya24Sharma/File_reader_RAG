13b4a3130c59ba1124ec5f4d4544288cf757395c
Aditya Sharma <aditya2002.05.19@gmail.com>
2025-03-11 18:52:51 -0500

    add: prev chats visible

--- frontend/src/app/components/chatbox.jsx
+++ frontend/src/app/components/chatbox.jsx
@@ -2,15 +2,19 @@
 import { useState, useEffect } from "react";
 import { fetchApi } from "../api/api";
 
+
+
 export default function Chatbox() {
     const [userMessage, setUserMessage] = useState("");
     const [botMessage, setBotMessage] = useState("");
     const [userInputValue, setUserInputValue] = useState("");
+    const [messages, setMessages] = useState([]);
 
 
     const handleSubmit = async (e) => {
         setUserInputValue("");
         e.preventDefault();
+        setMessages(prevMessages => [...prevMessages, {role: 'user', content: userInputValue}]);
         setUserMessage(userInputValue);
         const response = await fetchApi("query", {
             method: "POST",
@@ -18,6 +22,7 @@ export default function Chatbox() {
         });
         const data = await response.answer;
         console.log(data);
+        setMessages(prevMessages => [...prevMessages, {role: 'assistant', content: data}]);
         setBotMessage(data);
     }
     useEffect(() => {
@@ -26,13 +31,12 @@ export default function Chatbox() {
 
     return (
         <div className="p-2 border-2 border-gray-300 rounded-md bg-gray-900 h-full">
-            <div className="w-full h-[calc(100%-50px)] py-1 mb-2 flex flex-col gap-2 text-sm">
-                {userMessage && <div className="w-[80%] bg-gray-200 rounded-sm p-1 px-2 ml-auto mt-0 text-black ">
-                    {userMessage}
-                </div>}
-                {botMessage && <div className="w-[80%] bg-blue-500 rounded-sm p-1 px-2 mr-auto mt-0 text-white">
-                    {botMessage}
-                </div>}
+            <div className="w-full h-[calc(100%-50px)] py-1 mb-2 flex flex-col gap-2 text-sm overflow-y-auto no-scrollbar">
+                {messages.map((message, index) => (
+                    <div key={index} className={`max-w-[80%] ${message.role === 'user' ? 'bg-gray-200 rounded-sm p-1 px-2 ml-auto mt-0 text-black' : 'bg-blue-500 rounded-sm p-1 px-2 mr-auto mt-0 text-white'}`}>
+                        {message.content}
+                    </div>
+                ))}
             </div>
             <form onSubmit={handleSubmit} className="w-full h-10 border-2 border-gray-300 rounded-md bg-gray-900">
                 <input 
