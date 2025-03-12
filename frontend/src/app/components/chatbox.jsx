50a9c5a220dea7ffe2ab4058cfa13233b3d98888
Aditya Sharma <aditya2002.05.19@gmail.com>
2025-03-11 19:01:37 -0500

    update: chat scroll down

--- frontend/src/app/components/chatbox.jsx
+++ frontend/src/app/components/chatbox.jsx
@@ -1,37 +1,36 @@
 'use client'
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { fetchApi } from "../api/api";
 
 
 
 export default function Chatbox() {
-    const [userMessage, setUserMessage] = useState("");
-    const [botMessage, setBotMessage] = useState("");
     const [userInputValue, setUserInputValue] = useState("");
     const [messages, setMessages] = useState([]);
+    const chatRef = useRef(null);
 
+    useEffect(() => {
+        if (chatRef.current) {
+            chatRef.current.scrollTop = chatRef.current.scrollHeight;
+        }
+    }, [messages]);
 
     const handleSubmit = async (e) => {
         setUserInputValue("");
         e.preventDefault();
         setMessages(prevMessages => [...prevMessages, {role: 'user', content: userInputValue}]);
-        setUserMessage(userInputValue);
         const response = await fetchApi("query", {
             method: "POST",
             body: JSON.stringify({"query": e.target.message.value}),
         });
         const data = await response.answer;
-        console.log(data);
         setMessages(prevMessages => [...prevMessages, {role: 'assistant', content: data}]);
-        setBotMessage(data);
     }
-    useEffect(() => {
-        console.log(botMessage);
-    }, [botMessage]);
+
 
     return (
         <div className="p-2 border-2 border-gray-300 rounded-md bg-gray-900 h-full">
-            <div className="w-full h-[calc(100%-50px)] py-1 mb-2 flex flex-col gap-2 text-sm overflow-y-auto no-scrollbar">
+            <div ref={chatRef} className="w-full h-[calc(100%-50px)] py-1 mb-2 flex flex-col gap-2 text-sm overflow-y-auto no-scrollbar">
                 {messages.map((message, index) => (
                     <div key={index} className={`max-w-[80%] ${message.role === 'user' ? 'bg-gray-200 rounded-sm p-1 px-2 ml-auto mt-0 text-black' : 'bg-blue-500 rounded-sm p-1 px-2 mr-auto mt-0 text-white'}`}>
                         {message.content}
